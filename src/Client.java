9d24f3e4ac014f1082da736789968e25f8034b08
EmilienCompteEtude <emilien.cousin@uphf.fr>
2025-12-05 15:25:19 +0100

    Rename Client class to ClientChat and add main method

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,17 @@
-public class Client {
-   
-}
+public class ClientChat {
+
+	public static void main(String[] args) {
+		try {
+			ServiceChat sc = (ServiceChat) Naming.lookup("ServicesObjetDistant");
+			
+			System.out.println("Client connecté au RMI");
+			
+			
+			
+			
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
